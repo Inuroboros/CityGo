189efa2fee7bd56be0cd1a601c709943f2ce0a07
Ros9 <63921220+Ros9@users.noreply.github.com>
2021-09-26 23:40:41 +0600

    Add files via upload

---
+++ logging-api/src/test/java/com/example/loggingapi/LoggingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loggingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
