e92a2719102ffef894c65ebc20df4e156509cadd
inuroboros <alisherayupov10@gmail.com>
2021-10-24 14:45:00 +0600

    lombok and swagger2

--- user-microservice/src/main/java/user_microservice/user/model/User.java
+++ user-microservice/src/main/java/user_microservice/user/model/User.java
@@ -1,11 +1,16 @@
 package user_microservice.user.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
