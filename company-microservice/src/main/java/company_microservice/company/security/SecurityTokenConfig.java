d88e8965d4400317a64a635ce5c75da191a67887
inuroboros <alisherayupov10@gmail.com>
2021-11-14 23:49:13 +0600

    zuul

---
+++ company-microservice/src/main/java/company_microservice/company/security/SecurityTokenConfig.java
@@ -0,0 +1,31 @@
+package company_microservice.company.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+    private JwtConfig jwtConfig;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                    .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+                .and()
+                    .exceptionHandling().authenticationEntryPoint( ((httpServletRequest, httpServletResponse, e) -> httpServletResponse.sendError( HttpServletResponse.SC_UNAUTHORIZED )) )
+                .and()
+                    .addFilterAfter( new JwtTokenAuthenticationFilter( jwtConfig ), UsernamePasswordAuthenticationFilter.class )
+                .authorizeRequests()
+                    .antMatchers( HttpMethod.POST, jwtConfig.getUri());
+    }
+
+    public JwtConfig jwtConfig() {return new JwtConfig();}
+}
