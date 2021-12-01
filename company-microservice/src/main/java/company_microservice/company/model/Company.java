44f6d7f8f8b0bbcb4e511fbfa778254cd544b796
Alibi <alibisemeikhan@gmail.com>
2021-12-01 21:06:32 +0600

    сщьзфтн

--- company-microservice/src/main/java/company_microservice/company/model/Company.java
+++ company-microservice/src/main/java/company_microservice/company/model/Company.java
@@ -1,15 +1,10 @@
 package company_microservice.company.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
 @Data
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "company")
 public class Company {
     @Id
--- company-microservice/src/main/java/company_microservice/company/service/CompanyService.java
+++ company-microservice/src/main/java/company_microservice/company/service/CompanyService.java
@@ -1,56 +1,23 @@
 package company_microservice.company.service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import company_microservice.company.DAO.CompanyDAO;
 import company_microservice.company.model.Company;
 import company_microservice.company.model.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.http.HttpEntity;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@EnableHystrixDashboard
-@EnableHystrix
 public class CompanyService {
     @Autowired
     private CompanyDAO companyDAO;
 
-
     @Autowired
     private RestTemplate restTemplate;
-
-    @HystrixCommand(fallbackMethod = "getCompanyBIKByIdFallBack")
-    public String getCompanyBIKById(Long id){
-        return restTemplate.getForObject("http://companyapplication/company/bik/" + id, String.class);
-    }
-
-    @HystrixCommand(
-            fallbackMethod = "getCompanyNameByIdFallback",
-            threadPoolKey = "getCompanyNameById",
-            threadPoolProperties = {
-                    @HystrixProperty(name="coreSize", value="100"),
-                    @HystrixProperty(name="maxQueueSize", value="50"),
-                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
-                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "75"),
-                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "7000"),
-                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "15000"),
-                    @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "5")
-            }
-    )
-    public Company getCompanyNameById(Long id){
-        return restTemplate.getForObject("http://companyapplication/company/companyName/" + id, Company.class);
-    }
-
 
     public void createLog(Long userId, String action, String description){
         Log log = new Log(userId, "CompanyService", action, description);
@@ -90,17 +57,6 @@ public class CompanyService {
         return companyDAO.findCompanyByAddress(address);
     }
 
-    public String getCompanyBIKByIdFallBack(Long id){
-        return "BIK is not available";
-    }
-
-    public Company getCompanyNameByIdFallback(Long id){
-        Company company = new Company(0L,"Name is not available", "0000000", "Not available" );
-//        company.setId(101L);
-//        company.setCompanyName("Name is not available");
-        return company;
-    }
-
     public void createCompany(Company company){
         createLog(1L, "POST", "Created: " + company.toString() );
         companyDAO.save(company);
@@ -111,31 +67,8 @@ public class CompanyService {
         companyDAO.deleteById(id);
     }
 
-
-    public List<Company> getAllCompany() {
-        List<Company> companyList = new ArrayList<>();
-        List<Long> companyIds = new ArrayList<>();
-        for (Long id : companyIds) {
-            Company company = getCompanyNameById(id);
-            String BIK = getCompanyBIKById(id);
-            company.setBIK(BIK);
-            companyList.add(company);
-        }
-        return companyList;
-    }
-
     public Optional<Company> findById(Long id) {
         createLog(1L, "GET", "Get company, id = " + id);
         return companyDAO.findById(id);
-
     }
 }
-
-
-
-
-/*
-
-
-
-* */
